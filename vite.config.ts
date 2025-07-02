6cdb6183d20079b87c3fef1abab1af29d477a64e
Neil Sarkar <83109+neilsarkar@users.noreply.github.com>
2025-07-02 13:40:24 -0700

    Add a base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
@@ -17,6 +16,7 @@ export default defineConfig({
     }),
     vueDevTools(),
   ],
+  base: '/widgeteditor',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
